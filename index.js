e6f14e39e32d78a0e8602b1fc9f2cb226731ac0a
Sander van Rijsoort <s.rijsoort@gmail.com>
2023-09-01 19:22:20 +0100

    another test

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 app.all("/", (req, res) => {
   console.log("Just got a request!");
-  res.send("Yo!!!!");
+  res.send("This is and api endpoint with deployment on cyclic.sh");
 });
 app.listen(process.env.PORT || 3000);
